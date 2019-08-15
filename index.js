70b8c3460b41b10adda2359e00abe6f1aeb77ce8
Anthony <anthonywan00@gmail.com>
2019-08-15 15:59:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    let upperCaseLetter = string.charAt().toUpperCase();
+    return upperCaseLetter + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, "");
+  }
+
+  static titleize(string) {
+    let keywords = [
+      "the",
+      "a",
+      "but",
+      "an",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from"
+    ];
+    let word = string.split(" ");
+    word[0] = this.capitalize(word[0]);
+    return word
+      .map(w => {
+        if (keywords.includes(w)) {
+          return w;
+        } else if (word[0] === w) {
+          return this.capitalize(w);
+        } else {
+          return this.capitalize(w);
+        }
+      })
+      .join(" ");
+  }
 }
